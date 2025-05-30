f2a486ddfd82e64806832eedfa2a40908fc7f549
Umar Adelowo <umaradelo1.247@gmail.com>
2025-05-30 15:45:19 +0100

    Represented the lower, upper and step as symbolic constants in the program and not magic numbers

--- 01/fahrenheit.c
+++ 01/fahrenheit.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+#define LOWER 0 /* lower limit of table */
+#define UPPER 300 /* upper limit */
+#define STEP 20 /* step size */
 /**
  * print Fahrenheit-Celcius table for fahr = 0, 20, ..., 300
  */
@@ -7,6 +9,6 @@
 int main() {
   int fahr;
 
-  for (fahr = 0; fahr <= 300; fahr += 20)
+  for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
     printf("%3d\t%6.1f", fahr, (5.0 / 9.0) * (fahr - 32));
 }
