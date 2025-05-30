1d82e5a809871527723b6893e6b2e45a7683cb0d
Umar Adelowo <umaradelo1.247@gmail.com>
2025-05-30 16:17:21 +0100

    Formatted fahrenheit to adhere to good coding style

--- 01/fahrenheit.c
+++ 01/fahrenheit.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-#define LOWER 0 /* lower limit of table */
+#define LOWER 0   /* lower limit of table */
 #define UPPER 300 /* upper limit */
-#define STEP 20 /* step size */
+#define STEP 20   /* step size */
+
 /**
  * print Fahrenheit-Celcius table for fahr = 0, 20, ..., 300
  */
