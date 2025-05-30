06e279f79ec3fc9da30c29bf47979d821d0d5250
Umar Adelowo <umaradelo1.247@gmail.com>
2025-05-30 15:11:45 +0100

    modified the fahrenheit to celcius converter program to print a heading above the table

--- 01/fahrenheit.c
+++ 01/fahrenheit.c
@@ -5,17 +5,5 @@
  */
 
 int main() {
-  float fahr, celcius;
-  int lower, upper, step;
-
-  lower = 0;   /* lower limit of temperature table */
-  upper = 300; /* upper limit */
-  step = 20;   /* step size */
-
-  fahr = lower;
-  while (fahr <= upper) {
-    celcius = 5 * (fahr - 32) / 9;
-    printf("%3.0f\t%6.1f\n", fahr, celcius);
-    fahr += step;
-  }
+int
 }
