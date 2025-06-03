7e06e253b07d4897aafad14d65159b821ed88235
Umar Adelowo <umaradelo1.247@gmail.com>
2025-06-03 13:27:51 +0100

    Fixed a bug in the basic wc implementation

--- 01/basicwc.c
+++ 01/basicwc.c
@@ -14,14 +14,14 @@ int main() {
   nl = nw = nc = 0;
   while ((c = getchar()) != EOF) {
     ++nc;
-    if (c == nl)
+    if (c == '\n')
       ++nl;
     if (c == ' ' || c == '\t' || c == '\n')
       state = OUT;
-    else {
+    else if (state == OUT) {
       state = IN;
       ++nw;
     }
-    printf("%d %d %d\n", nl, nw, nc);
   }
+  printf("%d %d %d\n", nl, nw, nc);
 }
