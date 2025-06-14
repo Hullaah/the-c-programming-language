ea8e3d47eb990b84d2394404dcc2dd136c38d219
Umar Adelowo <umaradelo1.247@gmail.com>
2025-06-14 13:35:07 +0100

    Added doc for getbits

--- 02/getbits.c
+++ 02/getbits.c
@@ -8,6 +8,13 @@ int main()
 	printf("%#x\n", getbits(0xfde678, 11, 4));
 }
 
+/**
+ * getbits - get n bits of x starting at position p
+ * @x: the original number
+ * @p: the position to start getting bits (0-indexed from the right)
+ * @n: the number of bits to get
+ * Returns: the n bits from x starting at position p
+ */
 unsigned getbits(unsigned x, int p, int n)
 {
 	return (x >> (p + 1 - n)) & ~(~0 << n);
